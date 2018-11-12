1944aa44bdbc2454509b1350977b408d289d799c
HeorhiyHryhorov21 <george_best2001@mail.ru>
2018-11-12 15:00:05 +0200

    Add files via upload

--- estimate/js/main.js
+++ estimate/js/main.js
@@ -4,13 +4,32 @@ jQuery(document).ready(function() {
     that.tooltip('show');
     setTimeout(function(){
         that.tooltip('hide');
-    }, 2000);
+    }, 3000);
 });
 
 	jQuery('[data-toggle="tooltip"]').mouseleave(function(){
 	    jQuery(this).tooltip('hide');
 	});
 
-	
+	jQuery('#prst_select').change(function() {
+		if(jQuery('#prst_select').attr("value") != "0"){
+			jQuery('.add_new_preset').slideUp('slow');
+			jQuery('[name="add_save"]').hide();
+			jQuery('[name="add"]').css('display', 'block');
+		} else 
 
+		if (jQuery('#prst_select').attr("value") == "0") {
+			jQuery('.add_new_preset').slideDown('slow');
+			jQuery('[name="add_save"]').show();
+			jQuery('[name="add"]').css('display', 'none');
+		}
+	});
+
+	 jQuery('#timepicker1').timepicker({
+                template: false,
+                showMeridian: false,
+                maxHours: 72
+	 });
+
+	
 });
